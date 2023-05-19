81f1b67af3c505e0adda685b15844111fd10f37e
Takeuchi <g.takeuchi@gmail.com>
2023-05-18 21:35:54 -0300

    Some renames

--- cimple-back/src/cimple_back/__main__.py
+++ cimple-back/src/cimple_back/__main__.py
@@ -7,7 +7,7 @@ def main():
     if os.environ.get("STORE_URL") is None and os.environ.get("RUN_WITHOUT_DATABASE") is None:
         raise BaseException("STORE_URL is not set. If you want to run without database, set variable "
                             "RUN_WITHOUT_DATABASE")
-    uvicorn.run("cimple_back.app:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("cimple_back.cimple_back:app", host="0.0.0.0", port=8000, reload=True)
 
 
 if __name__ == "__main__":
--- cimple-store/src/cimple_store/__main__.py
+++ cimple-store/src/cimple_store/__main__.py
@@ -13,7 +13,7 @@ def main():
     db_file = os.environ.get("DB_FILE", "data/tasks.json")
     os.makedirs(os.path.dirname(db_file), exist_ok=True)
     port = os.environ.get("PORT", "8001")
-    uvicorn.run("cimple_store.app:app", host="0.0.0.0", port=int(port), reload=True)
+    uvicorn.run("cimple_store.cimple_store:app", host="0.0.0.0", port=int(port), reload=True)
 
 
 if __name__ == "__main__":
