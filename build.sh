0fb44891c3ef15f4bf818346cd7f456f41e53f4f
Takeuchi <g.takeuchi@gmail.com>
2023-06-27 21:17:16 -0300

    Adds scripts to build

--- build.sh
+++ build.sh
@@ -4,14 +4,16 @@ set -e
 
 VERSION=$(git branch --show-current)
 
+
 ARCH=$(uname -m)
-for D in $(ls -1d --color=none cimple-*)
+for D in $(ls -1d --color=none cimple-* )
 do
     echo $D
     if [ ! -d $D ]; then
         continue
     fi
     cd $D
+
     docker build -t taksan/$D:$VERSION-$ARCH .
     docker push taksan/$D:$VERSION-$ARCH 
     cd ..
